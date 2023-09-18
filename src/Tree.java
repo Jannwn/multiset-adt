f9c740e164081e3f24aece9bd8c9eb15413828f2
anstlgus02 <chloe.moon@mail.utoronto.ca>
2023-09-18 10:58:30 -0400

    __init__

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 public class Tree {
+    private Object root;
+    private List<Tree> subtrees;
+
+    public Tree(Object root) {
+        this(root, null);
+    }
+
+    public Tree(Object root, List<Tree> subtrees) {
+        this.root = root;
+        if (subtrees == null) {
+            this.subtrees = new ArrayList<>();
+        } else {
+            this.subtrees = new ArrayList<>(subtrees);
+        }
+    }
+
+    public Object getRoot() {
+        return root;
+    }
+
+    public List<Tree> getSubtrees() {
+        return subtrees;
+    }
 }
+
